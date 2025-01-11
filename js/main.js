181ffec361a35beda4b01f2bafcd554acad02d63
Devin <threateninglyamusing@gmail.com>
2025-01-11 04:36:55 -0800

    -

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ function generateCharacterDetailPage(characterData, data) {
     detailsContainer.innerHTML = ''; // Clear any existing content
 
     details.forEach(detail => {
-        // const listItem = document.createElement('li');
+        const listItem = document.createElement('p');
         listItem.innerHTML = `<strong>${detail.label}:</strong> ${detail.value}`;
         detailsContainer.appendChild(listItem);
     });
